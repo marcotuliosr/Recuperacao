2db530340a70c8dc2927692981c6a86cee5af878
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2021-01-21 10:52:18 -0300

    Configurações Gerais. Criando serviços

---
+++ src/main/java/br/com/recuperacao/service/MusicaService.java
@@ -0,0 +1,13 @@
+package br.com.recuperacao.service;
+
+import br.com.recuperacao.domain.Musica;
+
+import java.util.List;
+
+public interface MusicaService {
+    void salvar(Musica musica, long playlistId);
+    List<Musica> recuperarPorPlaylist(long playlistId);
+    Musica recuperarPorPlaylistIdEMusicaId(long playlistId, long musicaId);
+    void atualizar(Musica musica, long playlistId);
+    void excluir(long playlistId, long musicaId);
+}
---
+++ src/main/java/br/com/recuperacao/service/PlaylistService.java
@@ -0,0 +1,16 @@
+package br.com.recuperacao.service;
+
+import br.com.recuperacao.domain.Playlist;
+
+import java.util.List;
+
+public interface PlaylistService {
+
+    void salvar(Playlist playlist);
+    List<Playlist> recuperar();
+    Playlist recuperarPorId(long id);
+    void atualizar(Playlist playlist);
+    void excluir(long id);
+
+}
+
